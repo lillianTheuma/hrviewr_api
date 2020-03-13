187999f83acaa1fea822946cebc6dc3ef2f1b212
Student <example@example.com>
2020-03-13 08:46:23 -0700

    added task scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :employee
   belongs_to :business_process
+
+  scope :active, -> { where("completed == false")}
+  scope :withinOneWeek, -> { where ("created_at > ?", 7.days.ago)}
+  scope :monthly, -> { where ("created_at > ?", DateTime.now.beginning_of_month)}
+  scope :after, ->(comparator) { where ("created_at > ?", comparator)}
 end
